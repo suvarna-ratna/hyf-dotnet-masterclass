ede4a34252f7599586eea228bf978150ee744b42
suvarna <suvarna.akkala@gmail.com>
2023-03-14 13:26:50 +0100

    changes

--- week3/class3/Program.cs
+++ week3/class3/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/account", () => "Hello World!");
+
 
 app.Run();
